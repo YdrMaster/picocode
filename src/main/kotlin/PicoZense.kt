2e30b3ff135d200da3dffc3d07c5919a8ae8ee41
Ydr <ydrml@hotmail.com>
2019-12-26 12:38:34 +0800

    打不开流

--- src/main/kotlin/PicoZense.kt
+++ src/main/kotlin/PicoZense.kt
@@ -1,6 +1,9 @@
+import PicoZense.NativeFunctions.PsDeviceInfo
 import com.sun.jna.Library
 import com.sun.jna.Pointer
 import com.sun.jna.Structure
+import com.sun.jna.Structure.FieldOrder
+import com.sun.jna.ptr.ByteByReference
 import com.sun.jna.ptr.IntByReference
 import java.io.Closeable
 import java.lang.reflect.Proxy
@@ -26,6 +29,38 @@ object PicoZense : Closeable {
         return count.value
     }
 
+    operator fun get(index: Int): Pair<String, String> {
+        val info = PsDeviceInfo()
+        require(OK == native.Ps2_GetDeviceInfo(info, index))
+        val (name, code) =
+            info.uri.takeWhile { it != 0.toByte() }.toByteArray().toString(Charsets.US_ASCII).split(':')
+        return name to code
+    }
+
+    fun open(uri: String): Byte {
+        val handler = ByteByReference()
+        require(OK == native.Ps2_OpenDevice(uri, handler.pointer))
+        return handler.value
+    }
+
+    fun close(handler: Byte) {
+        val pointer = ByteByReference()
+        pointer.value = handler
+        require(OK == native.Ps2_CloseDevice(pointer.pointer))
+    }
+
+    fun start(handler: Byte) {
+        val pointer = ByteByReference()
+        pointer.value = handler
+        require(OK == native.Ps2_StartStream(pointer.pointer, 0))
+    }
+
+    fun stop(handler: Byte) {
+        val pointer = ByteByReference()
+        pointer.value = handler
+        require(OK == native.Ps2_StopStream(pointer.pointer, 0))
+    }
+
     override fun close() {
         native.Ps2_Shutdown()
     }
@@ -44,14 +79,30 @@ object PicoZense : Closeable {
 
         // 获取设备信息
         // pDevices for PsDeviceInfo*
-        fun Ps2_GetDeviceInfo(pDevices: Pointer, deviceIndex: Int): Int
+        fun Ps2_GetDeviceInfo(pDevices: PsDeviceInfo, deviceIndex: Int): Int
 
+        @FieldOrder("SessionCount", "devicetype", "uri", "fw", "status")
         class PsDeviceInfo : Structure() {
+            @JvmField
             var SessionCount: Int = -1
+            @JvmField
             var devicetype: Int = -1
+            @JvmField
             var uri = ByteArray(256) { -1 }
+            @JvmField
             var fw = ByteArray(50) { -1 }
-            var status: Int = -1
+            @JvmField
+            var status: Int = 0x0f
+
+            private fun uriPair() =
+                uri.takeWhile { it != 0.toByte() }
+                    .toByteArray()
+                    .toString(Charsets.US_ASCII)
+                    .split(':')
+                    .let { (name, code) -> name to code }
+
+            override fun toString() =
+                "$SessionCount, $devicetype, $status, ${uriPair()}"
         }
 
         // 打开设备
@@ -61,5 +112,13 @@ object PicoZense : Closeable {
         // 关闭设备
         // device for device handler
         fun Ps2_CloseDevice(device: Pointer): Int
+
+        // 启动数据流
+        // device for device handler
+        fun Ps2_StartStream(device: Pointer, sessionIndex: Int): Int
+
+        // 停止数据流
+        // device for device handler
+        fun Ps2_StopStream(device: Pointer, sessionIndex: Int): Int
     }
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -2,6 +2,12 @@ fun main() {
     PicoZense.use {
         it.runCatching {
             initialize()
+            val (name, code) = this[0]
+            val handler = open("$name:$code")
+            start(handler)
+            stop(handler)
+            close()
+            println("$name:$code")
         }
-    }.getOrElse { System.err.println(it.message) }
+    }.getOrElse { it.printStackTrace() }
 }
